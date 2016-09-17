e33dd8b128023d73c1fac7598508e113542feebe
butvin <butvan@gmail.com>
2016-09-17 21:09:58 +0300

    Refactoring

--- src/main/java/butvan/egestats/Main.java
+++ src/main/java/butvan/egestats/Main.java
@@ -3,17 +3,17 @@ package butvan.egestats;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Files.lines(Paths.get("data.txt"))
+        List<String> lines = Files.lines(Paths.get("data.txt"))
                 .filter(w -> Double.parseDouble(w.split(",")[2]) == 84)
-                .forEach(System.out::println);
-        OptionalDouble avg = Files.lines(Paths.get("data.txt"))
-                .filter(w -> Double.parseDouble(w.split(",")[2]) == 84)
-                .mapToDouble(w -> Double.parseDouble(w.split(",")[1]))
-                .average();
+                .collect(Collectors.toList());
+        lines.stream().forEach(System.out::println);
+        OptionalDouble avg = lines.stream().mapToDouble(w -> Double.parseDouble(w.split(",")[1])).average();
         System.out.println(avg.isPresent() ? "\nAverage for above: " + avg.getAsDouble() : "No matched lines");
     }
 }
