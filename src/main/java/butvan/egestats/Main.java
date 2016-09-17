8a5c32c2453ef0fbb19416c2202e897109189854
butvin <butvan@gmail.com>
2016-09-17 21:21:09 +0300

    Refactoring

--- src/main/java/butvan/egestats/Main.java
+++ src/main/java/butvan/egestats/Main.java
@@ -3,17 +3,19 @@ package butvan.egestats;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<String> lines = Files.lines(Paths.get("data.txt"))
-                .filter(w -> Double.parseDouble(w.split(",")[2]) == 84)
+        List<List<String>> lines = Files.lines(Paths.get("data.txt"))
+                .map(w -> Arrays.asList(w.split(",")))
+                .filter(w -> Double.parseDouble(w.get(2)) == 84)
                 .collect(Collectors.toList());
-        lines.stream().forEach(System.out::println);
-        OptionalDouble avg = lines.stream().mapToDouble(w -> Double.parseDouble(w.split(",")[1])).average();
+        lines.stream().map(w -> String.join(",", w)).forEach(System.out::println);
+        OptionalDouble avg = lines.stream().mapToDouble(w -> Double.parseDouble(w.get(1))).average();
         System.out.println(avg.isPresent() ? "\nAverage for above: " + avg.getAsDouble() : "No matched lines");
     }
 }
